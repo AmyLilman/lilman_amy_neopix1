6b1748faa40830237fc835531c8ec8c2a9f8094e
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2020-10-26 13:07:18 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 light.set_pixel_color(1, light.rgb(0, 0, 225))
+light.set_pixel_color(0, light.rgb(225, 0, 0))
--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
 light.setPixelColor(1, light.rgb(0, 0, 225))
+light.setPixelColor(0, light.rgb(225, 0, 0))
